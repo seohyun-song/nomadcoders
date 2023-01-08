5a80c7b3b2fdc139ee5083cee4d70b4b4856448a
seohyun-song <amanda94@naver.com>
2023-01-08 16:34:40 +0900

    commit

--- javascript-for-beginners/momentum/js/greetings.js
+++ javascript-for-beginners/momentum/js/greetings.js
@@ -1,6 +1,8 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
 const greeting = document.querySelector('#greeting');
+const todos = document.querySelector("#todo-form");
+const todosList = document.querySelector("#todo-list");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -13,11 +15,17 @@ function onSubmit(e) {
 
     localStorage.setItem(USERNAME_KEY, username);
     paintGreeting(username);
+    paintToDos();
 }
 
 function paintGreeting(username) {
-    greeting.innerText =`Hello ${username}` ;
+    greeting.innerText =`Hello, ${username}` ;
     greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+function paintToDos() {
+    todos.classList.remove(HIDDEN_CLASSNAME);
+    todosList.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
@@ -26,6 +34,7 @@ if(savedUsername === null) {
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener('submit', onSubmit); 
 } else {
-    paintGreeting(savedUsername)
+    paintGreeting(savedUsername);
+    paintToDos();
 }
 
