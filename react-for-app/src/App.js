19dbc931de8a7ee90516cff2095d3ad825836888
seohyun-song <amanda94@naver.com>
2023-03-07 15:24:24 +0900

    230307/ [edit]7.1 todo list part1
    useState

--- react-for-app/src/App.js
+++ react-for-app/src/App.js
@@ -1,19 +1,31 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-
-  function Hello() {
-    useEffect(() => {
-      console.log("created :)");
-      return () => console.log("destroyed :(");
-    }, []);
-    return <h1>Hello</h1>;
-  }
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    // 리액트에서는 state를 직접 수정하지 않음. modifier function 이용해서 수정
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="write your to do."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
